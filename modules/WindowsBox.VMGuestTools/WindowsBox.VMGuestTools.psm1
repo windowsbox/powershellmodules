ef4657775e139b2c69d96984e8e7bd10373cbfa4
Shawn Neal <sneal@sneal.net>
2016-12-28 19:21:59 -0800

    Add VM guest tools module

---
+++ modules/WindowsBox.VMGuestTools/WindowsBox.VMGuestTools.psm1
@@ -0,0 +1,27 @@
+<#
+.Synopsis
+    Install VM Guest Tools
+.Description
+    This cmdlet installs VM Guest tools for VirtualBox from a mounted CD
+#>
+function Install-VMGuestTools {
+    $installed = $false
+    foreach ($drive in Get-PSDrive -PSProvider 'FileSystem') {
+        $setup = "$($drive.Root)VBoxWindowsAdditions.exe"
+        $cert = "$($drive.Root)cert\oracle-vbox.cer"
+
+        if ((Test-Path $setup) -and (Test-Path $cert)) {
+            certutil -addstore -f 'TrustedPublisher' $cert
+
+            mkdir 'C:\Windows\Temp\virtualbox' -ErrorAction SilentlyContinue
+            Start-Process -FilePath $setup -ArgumentList '/S' -WorkingDirectory 'C:\Windows\Temp\virtualbox' -Wait
+
+            Remove-Item C:\Windows\Temp\virtualbox -Recurse -Force
+            $installed = $true
+        }
+    }
+
+    if (!$installed) {
+        Write-Error "Guest additions were not installed, did you forget to mount the ISO?"
+    }
+}
