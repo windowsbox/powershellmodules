eb6925a1977798eaf12b57d279ca21f5c37b86b9
Shawn Neal <sneal@sneal.net>
2016-12-28 16:49:42 -0800

    Add RDP module

---
+++ modules/WindowsBox.RDP/WindowsBox.RDP.psm1
@@ -0,0 +1,17 @@
+<#
+.Synopsis
+    Configures RDP
+.Description
+    This cmdlet configures Windows RDP
+#>
+function Configure-RDP {
+    # Enable RDP
+    Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Type DWord -Value 0
+
+    # Disable Network Level Authentication
+    $netauth = Get-WmiObject -Class Win32_TSGeneralSetting -ComputerName "." -Namespace root\CIMV2\TerminalServices -Authentication PacketPrivacy
+    $netauth.SetUserAuthenticationRequired(0)
+    
+    # Enable RDP on the firewall
+    Enable-NetFirewallRule -DisplayName 'Remote Desktop - User Mode (TCP-in)'
+}
