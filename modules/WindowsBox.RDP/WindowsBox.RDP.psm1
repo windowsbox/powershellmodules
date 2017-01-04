fe7ed5198f0f5e61a65bd33f3e1fec821ad88ab1
Shawn Neal <sneal@sneal.net>
2017-01-03 16:23:44 -0800

    Fix PSScriptAnalyzer warnings

--- modules/WindowsBox.RDP/WindowsBox.RDP.psm1
+++ modules/WindowsBox.RDP/WindowsBox.RDP.psm1
@@ -9,8 +9,8 @@ function Enable-RDP {
     Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Type DWord -Value 0
 
     # Disable Network Level Authentication
-    $netauth = Get-WmiObject -Class Win32_TSGeneralSetting -ComputerName "." -Namespace root\CIMV2\TerminalServices -Authentication PacketPrivacy
-    $netauth.SetUserAuthenticationRequired(0)
+    $ts = Get-CimInstance -Namespace root\cimv2\terminalservices -ClassName Win32_TSGeneralSetting -Filter 'TerminalName = "RDP-Tcp"'
+    $ts | Invoke-CimMethod -MethodName SetUserAuthenticationRequired -Arguments @{UserAuthenticationRequired=0} | Out-Null
     
     # Enable RDP on the firewall
     Enable-NetFirewallRule -DisplayName 'Remote Desktop - User Mode (TCP-in)'
